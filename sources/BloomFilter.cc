03564c9d7ea4b7f9f3f40b0647193fc8f9d4e86f
Louis Clergue <louisclergue@gmail.com>
2017-04-10 17:36:01 +0200

    basic string bloomfilter with universal hashing by default

--- sources/BloomFilter.cc
+++ sources/BloomFilter.cc
@@ -5,11 +5,23 @@ BloomFilter::BloomFilter(int m, int k)
   : m_size(m), k_hashes(k), bits(m,false) {}
   
 void BloomFilter::insert(const string a) {
-  size_t pos = hash<string>()(a) % m_size;
-  bits[pos] = true;
+  size_t ha = hash<string>()(a);
+  size_t h1 = ha & ((1 << 15) - 1); // first 15 bits
+  size_t h2 = ha >> 15; // other bits
+  for (int i = 0; i < k_hashes; ++i) {
+    size_t hi = (h1 + i*h2) % m_size;
+    bits[hi] = true;
+  }
 }
 
 bool BloomFilter::contains(const string a) const {
-  size_t pos = hash<string>()(a) % m_size;
-  return bits[pos];
+  bool present = true;
+  size_t ha = hash<string>()(a);
+  size_t h1 = ha & ((1 << 15) - 1); // first 15 bits
+  size_t h2 = ha >> 15; // other bits
+  for (int i = 0; present and i < k_hashes; ++i) {
+    size_t hi = (h1 + i*h2) % m_size;
+    present = bits[hi];
+  }
+  return present;
 }
--- sources/main.cc
+++ sources/main.cc
@@ -5,11 +5,20 @@
 using namespace std;
 
 int main() {
-  BloomFilter BF(10,1);
+  
+  size_t m, k;
+  cout << "give m and k :" << endl;
+  cin >> m >> k;
+  BloomFilter BF(m,k);
+  cout << "insert (+) or contains (?), (q) to quit" << endl;
+  char op;
   string input;
-  cout << "insert in bloom filter (0 to end): " << endl;
-  while (cin >> input and input != "0") BF.insert(input);
-  cout << "check contains (0 to end): " << endl;
-  while (cin >> input and input != "0")
-    cout << (BF.contains(input) ? "maybe in" : "not in") << endl;
+  while (cin >> op && op != 'q') {
+    cin >> input;
+    if (op == '+') BF.insert(input);
+    else if (op == '?')
+      cout << (BF.contains(input) ? "maybe in" : "not in") << endl;
+    else
+      cout << "wrong command" << endl;
+  }
 }
