28557d9703c3b5edec08dce4a0988fedcc8f5115
IrisBC <irisbc144@gmail.com>
2017-04-14 08:13:42 -0700

    Create MyExperiment1.h
    Creacio de estructura de test

---
+++ sources/MyExperiment1.h
@@ -0,0 +1,25 @@
+#ifndef __MYEXPERIMENT1_H__
+#define __MYEXPERIMENT1_H__
+#include "MyResult.h"
+#include <string>
+#include <vector>
+
+using std::string;
+using std::vector;
+
+class my_experiment_1 {
+
+public:
+  void exp_init();
+  void exp_do();
+  MyResult get_results();
+private:
+  vector<string> saved_data; // strings saved in the bloom filter
+  vector<string> chacked_data; // strings we look for in the bloom filter
+  int k_hash_functions;
+  int m_blom_size;
+  MyResult result;
+  
+};
+
+#endif
