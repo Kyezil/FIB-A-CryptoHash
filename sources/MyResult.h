a75cdd8bcb3bf8bfdb9d22bf3f2eee01b88631c7
IrisBC <irisbc144@gmail.com>
2017-04-14 08:12:35 -0700

    Create MyResult.h
    Struct de resultats

---
+++ sources/MyResult.h
@@ -0,0 +1,19 @@
+#ifndef __MYRESULT_H__
+#define __MYRESULT_H__
+#include <vector>
+#include <string>
+
+using std::string;
+using std::vector;
+
+struct MyResult {
+
+  int num_false;
+  int time_taken;
+  int k_hashes_used;
+  int m_size;
+  int n_strings;
+  
+};
+
+#endif
