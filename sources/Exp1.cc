39bb5567dbe6bf1b94198ac173cb57eefb8de15b
IrisBC <irisbc144@gmail.com>
2017-04-20 16:51:17 -0700

    Improve Exp1
    Add random strings to the test

--- sources/Exp1.cc
+++ sources/Exp1.cc
@@ -2,7 +2,41 @@
 #include <iostream>
 #include "BloomFilter.h"
 #include "Exp1.h"
+
+#include <random>
+#include <algorithm>
 using std::string;
+
+class RandomString {
+public:
+  static const char charset[];
+  static const size_t max_index;
+  
+  RandomString(int seed = 42) :
+    rand(seed), dis(0, max_index) {}
+  
+  string generate(size_t length) {
+    string str(length,0);
+    generate_n(str.begin(),length, [this]() -> char {
+      return randomChar();
+    });
+    return str;
+  }
+  
+private:
+  std::minstd_rand rand;
+  std::uniform_int_distribution<> dis;
+  inline char randomChar() {
+    return charset[dis(rand)];
+  }
+};
+
+const char RandomString::charset[] =
+  "abcdefghijklmnopqrstuvwxyz"
+  "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  "0123456789";
+const size_t RandomString::max_index = sizeof(RandomString::charset) - 1;
+
 
 Exp1::Exp1(string ex) {
   exampleParam = ex;
@@ -15,31 +49,28 @@ void Exp1::execute() {
   size_t m = 40, k = 1;
   res.m = m;
   res.k = k;
+  RandomString rs;
   BloomFilter BF(m,k);
   for (int i = 0; i < 40; ++i) {
-    BF.insert("CARACOL" + std::to_string(i)); // random string (o no tan random)
+    string ss = rs.generate(15);
+    //BF.insert("CARACOL" + std::to_string(i)); // random string (o no tan random)
+    BF.insert(ss);
     if (i%4 == 0) {                // deu hi hauran de ser segur
-      conjunt[i] = "CARACOL" + std::to_string(i);
+      conjunt[i] = ss;
     }
   }
-  /*for (int i = 0; i < 100; ++i) {
+  for (int i = 0; i < 100; ++i) {
     if (not (i%4 == 0 and i < 40)) {
-      string input;
-      //generate, com podem fer servir la classe que has creat de generacio aleatoria de coses?
-      conjunt[i] = input;
+      conjunt[i] = rs.generate(15);
     }
-  }*/
+  }
   int found = 0;
-  for (int i = 0; i < 100; ++i) { // paraules del conjunt quan estigui el conjunt ple
-    if (i%4 == 0 and i < 40) {
-      if (BF.contains(conjunt[i])) ++found;
-
-    }
-    else if (BF.contains("BANANAS" + std::to_string(i))) ++found;
+  for (int i = 0; i < 100; ++i) { 
+    if (BF.contains(conjunt[i])) ++found;
   }
   res.false_positive_ratio = found-10; // en teoria n'hi ha de haber exactament 10 per construccio
   res.n = 100;
-  std::cout << "Paraules trobades al BF: " << found << " de 100 buscades, 40 inserides i nomes 10 de les inserides en el conjunt d eles buscades :(" << std::endl;
+  std::cout << "Paraules trobades al BF: " << found << " de 100 buscades, 40 inserides i nomes 10 de les inserides en el conjunt de les buscades" << std::endl;
 }
 
 Experiment::Result Exp1::getResult() {
@@ -48,5 +79,5 @@ Experiment::Result Exp1::getResult() {
 
 // short description of the experiment
 string Exp1::getDescription() {
-  return "Experiment try 1, funciona millor amb un unic hash que amb mes d un";
+  return "Experiment try 1, un sol hash ja que m/n * 0,7 no arriba a 1";
 }
