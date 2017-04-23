282d654f092506437919c12f3501d0fe8500610b
Louis Clergue <louisclergue@gmail.com>
2017-04-23 11:13:06 +0200

    add dataGenerator definition

---
+++ src/experiments/include/DataGenerator.h
@@ -0,0 +1,19 @@
+#ifndef __DATA_GENERATOR_H__
+#define __DATA_GENERATOR_H__
+#include <istream>
+#include <string>
+using namespace std;
+
+class DataGenerator {
+public:
+  const string dataFolder;
+  // get istream of keys to insert
+  virtual istream & getKeys() {};
+  // get istream of i-th test
+  virtual istream & getTest(int i) {};
+  // get number of tests
+  virtual int size() const;
+};
+
+DataGenerator::dataFolder = "data";
+#endif
