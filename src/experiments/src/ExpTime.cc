295b12bd62686ff94f4960522f7fdf1bd42d03c2
Louis Clergue <louisclergue@gmail.com>
2017-04-23 21:47:46 +0200

    add ExpTime experiment

--- src/experiments/src/ExpTime.cc
+++ src/experiments/src/ExpTime.cc
@@ -3,7 +3,7 @@
 #include "RandomDataGenerator.h"
 #include "Execution.h"
 #include <fstream>
-#define BFS_N 5
+#define BFS_N 8
 
 ExpTime::ExpTime(int m, int kmin, int kmax, shared_ptr<RandomDataGenerator> dg)
 : m(m), kmin(kmin), kmax(kmax), times(BFS_N, vector<expTime> (max(0, kmax-kmin+1))), dg(dg) {}
@@ -16,11 +16,17 @@ void ExpTime::execute() {
     BF_fnv_jenkins bf2(m,k,false);
     BF_jenkins_murmur bf3(m,k,false);
     BloomFilter bf4(m,k,true);
+    BF_fnv_murmur bf5(m,k,true);
+    BF_fnv_jenkins bf6(m,k,true);
+    BF_jenkins_murmur bf7(m,k,true);
     execs[0] = Execution(&bf);
     execs[1] = Execution(&bf1);
     execs[2] = Execution(&bf2);
     execs[3] = Execution(&bf3);
     execs[4] = Execution(&bf4);
+    execs[5] = Execution(&bf5);
+    execs[6] = Execution(&bf6);
+    execs[7] = Execution(&bf7);
     for (int i = 0; i < BFS_N; ++i) {
       string s;
       ifstream keys;
@@ -35,7 +41,7 @@ void ExpTime::execute() {
         totalSize += res.test_size;
         containsTime += res.test_time;        
       }
-      times[i][k-kmin] = {insertTime/1000, containsTime/totalSize};
+      times[i][k-kmin] = {insertTime/700, containsTime/totalSize};
     }
   }
 }
@@ -43,7 +49,8 @@ void ExpTime::execute() {
 void ExpTime::displayResults() {
   cout << "Experiment Time results\n"
        << "insertion time\n"
-       << "k default fnv-murmur fnv-jenkins jenkins-murmur defaultSHA\n";
+       << "k default fnv-murmur fnv-jenkins jenkins-murmur defaultSHA"
+       " fnv-murmur-SHA fnv-jenkins-SHA jenkins-murmur-SHA\n";
   for (int i = 0; i < times[0].size(); ++i) {
     cout << kmin+i;
     for (int j = 0; j < times.size(); ++j) {
@@ -53,7 +60,8 @@ void ExpTime::displayResults() {
   }
   cout << endl;
   cout << "contains time\n"
-       << "k default fnv-murmur fnv-jenkins jenkins-murmur defaultSHA\n";
+       << "k default fnv-murmur fnv-jenkins jenkins-murmur defaultSHA"
+       " fnv-murmur-SHA fnv-jenkins-SHA jenkins-murmur-SHA\n";
   for (int i = 0; i < times[0].size(); ++i) {
     cout << kmin+i;
     for (int j = 0; j < times.size(); ++j) {
